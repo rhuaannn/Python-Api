ca0ae92a668dd442419f158845321a420cfc972f
rhuaannn <rhuanmarques1223@gmail.com>
2024-01-07 14:08:36 -0300

    update in url.

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from genres.views import GenreCreateListView,genre_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', GenreCreateListView.as_view, name='genre-create-list'),
+    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
     path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
 
 ]
--- genres/serializers.py
+++ genres/serializers.py
@@ -6,4 +6,4 @@ class GenreSerializer(serializers.ModelSerializer):
     
     class Meta:
         model =  Genre
-        fileds = '__all__'
+        fields = '__all__'
