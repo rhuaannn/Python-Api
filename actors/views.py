ac6c867de77a688c3738cc968b5f25ac8cddd391
rhuaannn <rhuanmarques1223@gmail.com>
2024-01-07 18:26:23 -0300

    feat: add url in actors.Model

--- actors/views.py
+++ actors/views.py
@@ -1,11 +1,11 @@
 from rest_framework import generics
 from actors.models import Actors
-
+from actors.serializers import ActorSerializer
 
 class ActorCreateListview(generics.ListCreateAPIView):
     queryset = Actors.objects.all()
-    serializer_class = None
+    serializer_class = ActorSerializer
 
 class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Actors.objects.all()
-    serializer_class = None
+    serializer_class = ActorSerializer
