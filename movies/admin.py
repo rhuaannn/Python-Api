d2ae37ee6ee92e99108476fda40e71b842fdc767
rhuaannn <rhuanmarques1223@gmail.com>
2024-01-13 11:56:10 -0300

    feat: create reviews

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from movies.models import Movies
+
+@admin.register(Movies)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genre', 'release_date','resume')
+    
--- movies/models.py
+++ movies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from actors.models import Actors
 from genres.models  import Genre
 
-class Movie(models.Model):
+class Movies(models.Model):
 
     genre = models.ForeignKey(Genre, on_delete = models.PROTECT, related_name='movies')
     title = models.CharField(max_length=500)
