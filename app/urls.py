52668f21cd3eb42bae56e0bd48468aff21537bd5
rhuaannn <rhuanmarques1223@gmail.com>
2024-01-13 11:00:37 -0300

    feat: add url movies

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from actors.views import ActorCreateListview, ActorRetrieveUpdateDestroyView
+from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,5 +13,7 @@ urlpatterns = [
     
     path('actors/', ActorCreateListview.as_view(), name='actor-create-list'),
     path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
- 
+
+    path('movies/', MovieCreateListView.as_view(), name='movies-create-list'),
+    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movies-detail-view'),
 ]
