be50651a55c37a6a7ab1211fcde8fba7f8873e1f
rhuaannn <rhuanmarques1223@gmail.com>
2024-01-13 10:55:48 -0300

    feat: create serializers

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework  import generics
+from movies.models import Movies
+from movies.serializers import MoviesSerializer
 
-# Create your views here.
+class MovieCreateListView(generics.ListCreateAPIView):
+    queryset = Movies.objects.all()
+    serializer_class = MoviesSerializer
+
+class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movies.objects.all()
+    serializer_class =  MoviesSerializer
