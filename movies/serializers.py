9508f2716b63e640edb7ad47ae3ea6312e646279
rhuaannn <rhuanmarques1223@gmail.com>
2024-01-14 19:11:18 -0300

    regra de negocio movies serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -5,3 +5,8 @@ class MoviesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movies
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year <= 1990:
+            raise serializers.ValidationError('Data inferior a 1990 não é permitido.')
+        return value
