12cc8056b97b729a1b60704f3cac690125535de1
rhuaannn <rhuanmarques1223@gmail.com>
2024-01-20 23:02:06 -0300

    validate em movies

--- movies/serializers.py
+++ movies/serializers.py
@@ -3,21 +3,24 @@ from movies.models import Movies
 
 from actors.serializers import ActorGetNameSerializer
 
-
-
 class MoviesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movies
         fields = '__all__'
 
+    def validate_release_date(self, value):
+        if value.year <= 1990:
+            raise serializers.ValidationError('Data inferior a 1990 não é permitido.')
+        return value
+
+    def validate_resume(self, value):
+        if len( value ) > 200:
+            raise serializers.ValidationError('Resume não deve ser maior que 200 caracteres.')
+        return value
 
 class MoviesGetSerializer(serializers.ModelSerializer):
     actors = ActorGetNameSerializer(many=True)
     class Meta:
         model = Movies
-        fields = ['title','release_date','resume','actors']
+        fields = ['id','title','release_date','resume','actors']
         
-    def validate_release_date(self, value):
-        if value.year <= 1990:
-            raise serializers.ValidationError('Data inferior a 1990 não é permitido.')
-        return value
