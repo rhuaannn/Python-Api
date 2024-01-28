5eba67af1d6113630fc15b47d5a7b80014c66660
rhuaannn <rhuanmarques1223@gmail.com>
2024-01-28 20:10:19 -0300

    feat: permission in view genres

--- genres/permissions.py
+++ genres/permissions.py
@@ -4,9 +4,16 @@ class GenrePermissionClass(permissions.BasePermission):
     
     def has_permission(self, request, view):
 
-        if request.method == 'GET':
+        if request.method in ['GET', 'OPTIONS', 'HEAD']:
             return request.user.has_perm('genres.view_genre')
 
         if request.method == 'POST':
             return request.user.has_perm('genres.add_genre')
+
+        if request.method in  ['PUT', 'PATCH']:
+            return request.user.has_perm('genres.change_genre')
+
+        if request.method == 'DELETE':
+            return request.user.has_perm('genres.delete_genre')
+        
         return False
