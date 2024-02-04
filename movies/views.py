ecbf4e7c1ca7f95d28d20dc153f980705e3233e3
rhuaannn <rhuanmarques1223@gmail.com>
2024-02-04 15:38:29 -0300

    add permission global nas views pendentes

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,13 @@
 from rest_framework  import generics
 from rest_framework.permissions import IsAuthenticated
- 
+from app.permission import GlobalDefaultPermission
+
 from movies.models import Movies
 from movies.serializers import MoviesGetSerializer, MoviesSerializer
  
 class MovieCreateListView(generics.ListCreateAPIView):
     
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,GlobalDefaultPermission)
     queryset = Movies.objects.all()
     serializer_class = MoviesSerializer
 
@@ -16,6 +17,6 @@ class MovieCreateListView(generics.ListCreateAPIView):
         return MoviesSerializer
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,GlobalDefaultPermission)
     queryset = Movies.objects.all()
     serializer_class =  MoviesSerializer
