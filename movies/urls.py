d8bd70f631a8916c8acf9e757694f7c3935928e5
rhuaannn <rhuanmarques1223@gmail.com>
2024-02-04 23:29:21 -0300

    feat: criando uma nova rota e uma view sem metodos prontos

--- movies/urls.py
+++ movies/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     
     path('movies/<int:pk>/', views.MovieRetrieveUpdateDestroyView.as_view(),
          name='movies-detail-view'),
+
+    path('movies/stats/', views.MovieStatsView.as_view(), name='movie-stats-view')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,6 @@
-from rest_framework  import generics
+from rest_framework  import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from app.permission import GlobalDefaultPermission
-
 from movies.models import Movies
 from movies.serializers import MoviesGetSerializer, MoviesSerializer
  
@@ -20,3 +19,15 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated,GlobalDefaultPermission)
     queryset = Movies.objects.all()
     serializer_class =  MoviesSerializer
+
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
+    queryset = Movies.objects.all()
+
+    def get(self, request):
+
+        return response.Response(
+            data = {'message': 'bateu aqui'},
+            status = status.HTTP_200_OK,
+        )
